e0af8e7fb79108985ac068368ce32fc0c483681c
Matt Williams <matt.seongjun@gmail.com>
2019-06-04 22:32:58 -0400

    Finish pet new action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(name: params[:name])
+    if params["owner_name"].empty?
+      @pet.owner = Owner.find_by_id(params[:owner_id])
+    else
+      @pet.owner = Owner.create(name: params[:owner_name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
